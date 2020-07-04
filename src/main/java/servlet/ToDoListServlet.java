7050ab6843832115dca990735f42377b8449b61f
Karen Gasparyan <mrkaren92@gmail.com>
2020-07-04 21:45:39 +0400

    add comment

--- src/main/java/servlet/ToDoListServlet.java
+++ src/main/java/servlet/ToDoListServlet.java
@@ -14,6 +14,7 @@ import java.util.List;
 @WebServlet(urlPatterns = "/todolist")
 public class ToDoListServlet extends HttpServlet {
 
+    //todoManager
     ToDoManager toDoManager = new ToDoManager();
 
     @Override
